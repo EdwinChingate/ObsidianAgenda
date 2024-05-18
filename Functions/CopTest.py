856650ad88ef1392edf1d0bbdb9f6433e5b20c14
eachingateb <eachingateb@unal.edu.co>
2024-05-18 13:28:50 -0500

    Filetes

---
+++ Functions/CopTest.py
@@ -0,0 +1,9 @@
+def Cop(old,new): #Coppy files from an old rute to a newone
+    #old: full name of the file with the current directory
+    #new: new name for the file with the directory wanted
+    origen=old
+    destino=new
+    if os.path.exists(origen):
+        with open(origen, 'rb') as forigen:
+            with open(destino, 'wb') as fdestino:
+                shutil.copyfileobj(forigen, fdestino)
